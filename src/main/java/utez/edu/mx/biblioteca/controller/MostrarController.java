e35cb9d3df267ab5e53b97d09813e54af0a45790
BrianGalvezchess <20253rd056@utez.edu.mx>
2026-04-16 21:30:28 -0600

    Agrego imports y atributos lblId lblTitulo lblGenero

--- src/main/java/utez/edu/mx/biblioteca/controller/MostrarController.java
+++ src/main/java/utez/edu/mx/biblioteca/controller/MostrarController.java
@@ -1,4 +1,23 @@
 package utez.edu.mx.biblioteca.controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 
 public class MostrarController {
+
+    @FXML
+    private Label lblId;
+
+    @FXML
+    private Label lblTitulo;
+    @FXML
+    private Label lblGenero;
+
+    @FXML
+    public void mostrar(){
+        lblId.setText("ID: " + Libro.getUltimoId());
+        lblTitulo.setText("Título: " + Libro.getUltimoTitulo());
+        lblGenero.setText("Género: " + Libro.getUltimoGenero());
+    }
 }
