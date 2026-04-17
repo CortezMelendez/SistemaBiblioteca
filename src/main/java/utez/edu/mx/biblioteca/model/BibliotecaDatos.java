dae4add50321869a13a8e51528a71f1c21bc0613
angel <>
2026-04-16 21:44:36 -0600

    Creacion de la clase para almacenar datos

---
+++ src/main/java/utez/edu/mx/biblioteca/model/BibliotecaDatos.java
@@ -0,0 +1,25 @@
+package utez.edu.mx.biblioteca.model;
+
+public class BibliotecaDatos {
+    private int id;
+    private String genero;
+    private String titulo;
+
+    public BibliotecaDatos(int id, String genero, String titulo) {
+        this.id = id;
+        this.genero = genero;
+        this.titulo = titulo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+}
