066fe11cd6ef84fad7dbe7c7e9ec82d32d2dd7ad
angel <>
2026-04-17 11:51:29 -0600

    Arreglo de errores con el controlador base

--- src/main/java/utez/edu/mx/biblioteca/controller/MostrarController.java
+++ src/main/java/utez/edu/mx/biblioteca/controller/MostrarController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import utez.edu.mx.biblioteca.model.BibliotecaDatos;
 
-public class MostrarController extends BibliotecaDatos {
+public class MostrarController extends ControladorBase {
 
 
     @FXML
